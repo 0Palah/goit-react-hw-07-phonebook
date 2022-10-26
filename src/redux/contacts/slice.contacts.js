dea4962ab7078a113a2f7c804b6627c33e183039
Oleg Palashynskyi <palah@ukr.net>
2022-10-26 19:56:51 +0300

    Update slice.contacts.js

--- src/redux/contacts/slice.contacts.js
+++ src/redux/contacts/slice.contacts.js
@@ -6,40 +6,40 @@ import {
   fetchContacts,
 } from 'redux/contacts/contactsThunks';
 
+const handlePending = (state, action) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsInitialState,
 
   extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.pending]: handlePending,
+    [addContact.pending]: handlePending,
+    [deleteContact.pending]: handlePending,
+
+    [fetchContacts.rejected]: handleRejected,
+    [addContact.rejected]: handleRejected,
+    [deleteContact.rejected]: handleRejected,
+
     [fetchContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
 
-    [addContact.pending](state) {
-      state.isLoading = true;
-    },
     [addContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
     },
-    [addContact.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
 
-    [deleteContact.pending](state) {
-      state.isLoading = true;
-    },
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -47,10 +47,6 @@ const contactsSlice = createSlice({
         contact => contact.id === action.payload.id
       );
       state.items.splice(index, 1);
-    },
-    [deleteContact.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
     },
   },
 });
